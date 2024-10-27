e5fddcfade9b953659e7749790b20bd6488a0bed
Alex874565 <alex.carpinisan@gmail.com>
2024-10-27 13:17:32 +0200

    Initial commit

---
+++ FrontEnd/src/static/landing.jsx
@@ -0,0 +1,14 @@
+import './landing.css';
+
+var landing = () => {
+    return (
+        <div id="landing-page">
+            <div className="wrapper">
+                <h1>AI Have A Secret</h1>
+                <h4>THE AI Security Game</h4>
+                <h4>Prompt Injection Attack/Defense</h4>
+            </div>
+        </div>);
+}
+
+export default landing;
