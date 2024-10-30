5b8e1e061cd5157a6fcafed007df054f3c6bfbb5
RoxiKeller <kellerroxana30@gmail.com>
2024-10-30 22:18:00 +0200

    am terminat landing

--- FrontEnd/src/static/landing.jsx
+++ FrontEnd/src/static/landing.jsx
@@ -1,26 +1,34 @@
-import {useEffect} from "react";
-import $ from 'jquery';
+import { useEffect } from "react";
+import $ from "jquery";
+import Navbar from "../components/navbar.jsx";
+import "./landing.css";
 
-import Navbar from '../components/navbar.jsx'
-import './landing.css';
+const Landing = () => {
+  useEffect(() => {
+    $("#navbar").children().each(function () {
+      $(this).removeClass("active");
+    });
+  }, []);
 
-var landing = () => {
+  return (
+    <div id="landing-page">
+      <div className="content-wrapper">
+        <h2>Welcome to</h2>
+        <h1>AI Have A Secret</h1>
+        <h2 className="subtitle">
+          The AI Prompt Injection<br className="line-break" /> Attack/Defense Game
+        </h2>
+      </div>
+      <div className="button-container">
+        <a href="/attack">
+            <button className="button button_att">Attack AI!</button>
+        </a>
+        <a href="/defend">
+            <button className="button button_def">Defend my own AI!</button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-    useEffect(() => {for(let child in $('#navbar').children) {child.classList.remove('active')}}, [])
-
-    return (
-        <div id={"landing-page"}>
-            
-            <div className="content-wrapper">
-                <h2>Welcome to</h2>
-                <h1>AI Have A Secret</h1>
-                <h2>The AI Prompt Injection Attack/Defense Game</h2>
-            </div>
-            <div>
-                <button class="button button_att">Attack AI!</button>
-                <button class="button button_def">Defend my own AI!</button>
-            </div>
-        </div>);
-}
-
-export default landing;
+export default Landing;
